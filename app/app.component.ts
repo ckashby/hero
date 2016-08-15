c70d52562b8be1f719abe37d214330253119c7de
ckashby <clay.ashby@gmail.com>
2016-08-15 16:25:46 -0700

    aster details

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,4 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { Component } from '@angular/core';  // Does not work without this
-import { AppComponent } from './app.component';
-
-@NgModule({
-	imports: [
-	BrowserModule,
-	FormsModule
-	],
-	declarations: [
-	  AppComponent
-	],
-	bootstrap: [ AppComponent ]
-})
-export class AppModule { }
+import { Component } from '@angular/core';  
 
 export class Hero {
 	id: number;
@@ -22,30 +6,99 @@ export class Hero {
 	ego: string;
 	story: string;
 }
+
+const HEROES: Hero[] = [
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' }
+];
+
 @Component({
-    selector: 'my-app',
-    template: `<h1>{{title}}</h1>
-    		   <h2>{{hero.name}}</h2>
-               <div>
-                 <label>Id: </label>{{hero.id}}</div>
-	               <div>
-	                 <label>Name: </label>
-	                 <input [(ngModel)]="hero.name"
-						  (ngModelChange)="hero.name=$event" placeholder="name">
-	                </div>`
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input value="{{selectedHero.name}}" placeholder="name"/>
+      </div>
+    </div>
+    <div class="myDiv">Hi</div>
+ `,
+styles: [`
+  .myDiv {
+    background-color: #CFD800 !important;
+    color: blue;
+    border: solid 2px green;
+    padding: 3em;
+    /* margin: 0 auto; */
+    display: flex; 
+    justify-content: center; 
+  }
+  .selected {
+    background-color: #CFD800 !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
-export class AppComponent { 
-  title = 'New Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-    ego: 'Jane Mui',
-    desc: 'A mysterious young asian woman.'
-  };
-  hero2: Hero = {
-    id: 2,
-    name: 'Mindstorm',
-    ego: 'Sara Colfax',
-    desc: 'A mysterious young black woman.'
-  };
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
